797725ccc707f3d77056c26640c0043b4aba3609
Noo8 Cod3r <noo8cod3r@noo8s-mbp.attlocal.net>
2019-05-24 13:57:19 -0500

    Ball Component/CSS created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import LotteryBall from './Components/LotteryBall';
 
 function App() {
   return (
     <div className="App">
-      
+      <LotteryBall num = { 22 }/>
     </div>
   );
 }
--- src/Components/LotteryBall.js
+++ src/Components/LotteryBall.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import './LotteryBall.css'
+
 
 
 export default class LotteryBall extends Component {
